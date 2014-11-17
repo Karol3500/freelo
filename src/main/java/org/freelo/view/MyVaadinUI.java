2aef32df4bf452f38092f6d5acaf66bef7261306
karol3500 <kposila@gmail.com>
2014-11-18 00:09:39 +0100

    Initial commit

---
+++ src/main/java/org/freelo/view/MyVaadinUI.java
@@ -0,0 +1,33 @@
+package org.freelo.view;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.spring.VaadinUI;
+import org.vaadin.spring.navigator.SpringViewProvider;
+
+/**
+ * Created by karol on 17.11.14.
+ */
+@VaadinUI
+public class MyVaadinUI extends UI {
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    SpringViewProvider viewProvider;
+    Navigator navigator;
+    protected static final String LOGINVIEW = "LoginView";
+    @Autowired
+    LoginView loginView;
+
+    @Override
+    public void init(VaadinRequest request) {
+        Page.getCurrent().setTitle("Example IT App");
+        navigator = new Navigator(this,this);
+        navigator.addProvider(viewProvider);
+        navigator.addView("",viewProvider.getView(LOGINVIEW));
+        //navigator.addView("", new LoginView(navigator));
+    }
+
+}

0e61857e637f7b18017d5c56b50dadfb19828ba6
karol3500 <kposila@gmail.com>
2014-12-01 12:16:35 +0100

    added session factory bean

---
+++ src/main/java/org/freelo/model/SessionFactoryBean.java
@@ -0,0 +1,32 @@
+package org.freelo.model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+
+/**
+ * Created by karol on 01.12.14.
+ */
+@Component
+@Scope("singleton")
+public class SessionFactoryBean {
+    static SessionFactory sessionFactory;
+    @PostConstruct
+    void construct(){
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static Session getSession(){
+        return sessionFactory.openSession();
+    }
+}

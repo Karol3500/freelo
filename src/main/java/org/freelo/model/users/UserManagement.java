cf813ad1bc4ac5f963355a836bb7b258b9113f32
artur <arturwaz11@gmail.com>
2014-12-07 09:22:30 +0100

    Updated User Management class with all necessary methods.

--- src/main/java/org/freelo/model/users/UserManagement.java
+++ src/main/java/org/freelo/model/users/UserManagement.java
@@ -4,12 +4,12 @@ package org.freelo.model.users;
 import org.freelo.model.HibernateSessionFactoryBean;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-//import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.Iterator;
 
 public class UserManagement {
+
+    // only for test do not use in final program !!!
     public void testDisplay(){
         Session session = HibernateSessionFactoryBean.getSession();
         try{
@@ -30,69 +30,133 @@ public class UserManagement {
 
         }catch (HibernateException e) {
             e.printStackTrace();
+            session.getTransaction().rollback();
         }finally {
             session.close();
         }
     }
 
-
-    public static User getUserByMail(String email){
+    // returns user ID if found, null if user doesnt exist
+    public static Integer getUserID(String email){
         Session session = HibernateSessionFactoryBean.getSession();
-        User user = new User();
+        Integer user = null;
         try{
             session.beginTransaction();
 
-            List users = session.createQuery("FROM User").list();
-            for (Iterator iterator = users.iterator(); iterator.hasNext();){
-
-                User temp = (User) iterator.next();
-                if (email.equals(temp.getEmail())) {
-                    user = temp;
-                    break;
-                }
-
-            }
+            List users = session.createQuery("FROM User U WHERE U.email = '"+email+"'").list();
+            if (!users.isEmpty())
+                user = ((User) users.get(0)).getId();
 
             session.getTransaction().commit();
         }catch (HibernateException e) {
             e.printStackTrace();
+            session.getTransaction().rollback();
         }finally {
             session.close();
         }
         return user;
     }
 
-
-    // add a new user
-    public static Integer userAdd(String fname, String lname, String email, String password){
+    // returns User if found, null if user doesnt exist
+    public static User getUser(String email){
         Session session = HibernateSessionFactoryBean.getSession();
-        Integer employeeID = null;
+        User user = null;
         try{
             session.beginTransaction();
-            User user = new User(fname, lname, email, password);
-            employeeID = (Integer) session.save(user);
+
+            List users = session.createQuery("FROM User U WHERE U.email = '"+email+"'").list();
+            if (!users.isEmpty())
+                user = (User) users.get(0);
+
             session.getTransaction().commit();
         }catch (HibernateException e) {
             e.printStackTrace();
+            session.getTransaction().rollback();
         }finally {
             session.close();
         }
-        return employeeID;
+        return user;
     }
 
-
-
-    // delete user from DB
-    public static void deleteUser(Integer EmployeeID){
+    // returns User if found, null if user doesnt exist
+    public static User getUser(Integer UserID){
         Session session = HibernateSessionFactoryBean.getSession();
+        User user = null;
         try{
             session.beginTransaction();
-            User user =
-                    (User)session.get(User.class, EmployeeID);
-            session.delete(user);
+
+            List users = session.createQuery("FROM User U WHERE U.id = "+UserID).list();
+            if (!users.isEmpty())
+                user = (User) users.get(0);
+
             session.getTransaction().commit();
         }catch (HibernateException e) {
             e.printStackTrace();
+            session.getTransaction().rollback();
+        }finally {
+            session.close();
+        }
+        return user;
+    }
+
+    // add a new user, return user ID, null if adding user failed (eg. user with such email exist in DB)
+    public static Integer userAdd(String fname, String lname, String email, String password){
+        Session session = HibernateSessionFactoryBean.getSession();
+        Integer userID = null;
+        try{
+            session.beginTransaction();
+
+            List users = session.createQuery("FROM User U WHERE U.email = '"+email+"'").list();
+            if (users.isEmpty()) {
+                User user = new User(fname, lname, email, password);
+                userID = (Integer) session.save(user);
+            }
+
+            session.getTransaction().commit();
+        }catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }finally {
+            session.close();
+        }
+        return userID;
+    }
+
+    // delete user from DB
+    public static void deleteUser(String email){
+        Session session = HibernateSessionFactoryBean.getSession();
+        try{
+            session.beginTransaction();
+
+            List users = session.createQuery("FROM User U WHERE U.email = '"+email+"'").list();
+            if (!users.isEmpty()) {
+                User user = (User)session.get(User.class, ((User) users.get(0)).getId());
+                session.delete(user);
+            }
+
+            session.getTransaction().commit();
+        }catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }finally {
+            session.close();
+        }
+    }
+
+    // delete user from DB
+    public static void deleteUser(Integer UserID){
+        Session session = HibernateSessionFactoryBean.getSession();
+        try{
+            session.beginTransaction();
+
+
+            User user = (User)session.get(User.class, UserID);
+            session.delete(user);
+
+            session.getTransaction().commit();
+        }catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
         }finally {
             session.close();
         }
@@ -103,16 +167,18 @@ public class UserManagement {
         Session session = HibernateSessionFactoryBean.getSession();
         try{
             session.beginTransaction();
-            User user =
-                    (User)session.get(User.class, ID);
+
+            User user = (User)session.get(User.class, ID);
             user.setName(fname);
             user.setLastName(lname);
             user.setEmail(email);
             user.setPassword(password);
             session.update(user);
+
             session.getTransaction().commit();
         }catch (HibernateException e) {
             e.printStackTrace();
+            session.getTransaction().rollback();
         }finally {
             session.close();
         }

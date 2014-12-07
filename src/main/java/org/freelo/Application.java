1a8902c90f3b929de833895bea4d84e9d875d4bc
artur <arturwaz11@gmail.com>
2014-12-07 09:45:25 +0100

    Hard coded userDB, will be romeved in future.

--- src/main/java/org/freelo/Application.java
+++ src/main/java/org/freelo/Application.java
@@ -1,5 +1,7 @@
 package org.freelo;
 
+import org.freelo.model.users.User;
+import org.freelo.model.users.UserManagement;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -15,5 +17,18 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        //todo delete in final version
+        // do not delete this part, it is only for testing
+        System.out.print("\n\n\n");
+        UserManagement manage = new UserManagement();
+        manage.userAdd("Artur","Waz","arturwaz@freelo.com","password");
+        manage.userAdd("Karol","Posila","karolposila@freelo.com","password");
+        manage.userAdd("Adrian","Cyga","adriancyga@freelo.com","password");
+        manage.userAdd("Konrad","Jazownik","konradjazownik@freelo.com","password");
+        manage.userAdd("Piotr","Bienias","piotrbienias@freelo.com","password");
+        manage.userAdd("Jan","Dziergwa","jandziergwa@freelo.com","password");
+        manage.userAdd("Ruben","Diaz","rubendiaz@freelo.com","password");
+
     }
 }

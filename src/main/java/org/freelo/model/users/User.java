ea80d8f7d620a5ce64f2803d0ebe66876234eb95
ArturWaz <arturwaz11@gmail.com>
2014-12-08 13:41:32 +0100

    Added users groups and privileges.

--- src/main/java/org/freelo/model/users/User.java
+++ src/main/java/org/freelo/model/users/User.java
@@ -2,7 +2,9 @@ package org.freelo.model.users;
 
 import javax.persistence.*;
 import javax.persistence.GeneratedValue;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 @Entity
@@ -28,7 +30,8 @@ public class User {
 	@Column
 	private Date created; // user's register time
 
-    //todo make list of privileges for user
+    //@OneToMany
+    //private List<Privilege> privileges = new ArrayList<>();
 
 	public User(){
 		firstName = "";
@@ -70,5 +73,15 @@ public class User {
 
 	public Date getDate() { return created; }
 	//public void setDate(Date createdDate) { this._created = createdDate; }
+
+    //todo write code for below functions
+    /*public void addPrivilege(int privilegeID) {
+        for (Privilege element : privileges) {
+            ;
+        }
+        //privileges.add()
+    }
+    public void deletePrivilege(int privilegeID) {}
+    public List<Privilege> getPrivileges() { return privileges; }*/
 
 }

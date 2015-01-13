d28761376fcb71e238f6beabe1922774211c3f50
dziergwajan <dziergwa.jan@gmail.com>
2015-01-13 17:04:12 +0100

    Project page: layout changes, added sprint creation window, added project management window (work in progress)

--- src/main/java/org/freelo/view/ProjectManagementPage.java
+++ src/main/java/org/freelo/view/ProjectManagementPage.java
@@ -29,6 +29,8 @@ public class ProjectManagementPage extends HorizontalLayout implements View{
     public static final String NAME = "Project Management";
     public ProjectManagementPageController pageController;
     public Subwindow createNewProj;
+    public addSprintWindow createNewSprint;
+    public manageProjectWindow manageProject;
 
     @Autowired
     DashboardMenuBean dashboardMenuBean;
@@ -61,6 +63,10 @@ public class ProjectManagementPage extends HorizontalLayout implements View{
     public void addProject(String email, String name) {
         container2.addComponent(new ProjectItem(name, email));
     }
+
+
+///////////////////// Project Window
+
 
     public class Subwindow extends Window {
         private static final long serialVersionUID = 5678234591401040269L;
@@ -103,14 +109,120 @@ public class ProjectManagementPage extends HorizontalLayout implements View{
 
         }
 
+
+
         public ProjectItem createProject(TextField projectName, VerticalLayout container2) {
             final String name = projectName.getValue();
-            ProjectItem pi = new ProjectItem(name);
-
+            String manager = (String) VaadinSession.getCurrent().getAttribute("user");
+            ProjectItem pi = new ProjectItem(name, manager);
             container2.addComponent(pi);
             return pi;
         }
 
+
+    }
+
+
+
+///////////////////// Sprint Window
+
+
+    public class addSprintWindow extends Window {
+        private static final long serialVersionUID = 5683290459141040269L;
+        //        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        //public Button createButton;
+        //public ProjectItem pi;
+        //CreateProjectSubwindowController c;
+
+        public addSprintWindow(final VerticalLayout nextcontainer) {
+            super("New sprint");
+            //c = new CreateProjectSubwindowController(this);
+            center();
+            HorizontalLayout main = new HorizontalLayout();
+            main.addStyleName("projectpopup");
+            main.setSizeFull();
+            setContent(main);
+            setHeight("300px");
+            setWidth("300px");
+            setPositionY(50);
+            setPositionX(50);
+
+            final TextField ProjectName = new TextField("Enter sprint name");
+            ProjectName.focus();
+            // todo.add date
+            PopupDateField startDatePicker = new PopupDateField("Start date");
+            PopupDateField endDatePicker = new PopupDateField("End date");
+
+            Button createButton = new Button("Create", new Button.ClickListener() {
+                private static final long serialVersionUID = 2181474159749122119L;
+
+                @Override
+                public void buttonClick(Button.ClickEvent event) {
+                    //pi = createProject(ProjectName, container2);
+                    close();
+                    //c.createProject(pi);
+
+                    SprintViewObject sprint = new SprintViewObject();
+                    nextcontainer.addComponent(sprint.SprintButton);
+                    sprint.SprintButton.addStyleName("SprintButton");
+                    sprint.SprintButton.setWidth("100%");
+                }
+            });
+            createButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+            VerticalLayout itemplacement = new VerticalLayout();
+            itemplacement.setSizeFull();
+            itemplacement.addComponent(ProjectName);
+            itemplacement.addComponent(startDatePicker);
+            itemplacement.addComponent(endDatePicker);
+            itemplacement.addComponent(createButton);
+            main.addComponent(itemplacement);
+
+        }
+
+    }
+
+
+///////////////////// Manage Project Window
+
+    public class manageProjectWindow extends Window {
+        private static final long serialVersionUID = 5683290459141040269L;
+        //        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        //public Button createButton;
+        //public ProjectItem pi;
+        //CreateProjectSubwindowController c;
+
+        public manageProjectWindow() {
+            super("Project");
+            //c = new CreateProjectSubwindowController(this);
+            center();
+            HorizontalLayout main = new HorizontalLayout();
+            main.addStyleName("projectpopup");
+            main.setSizeFull();
+            setContent(main);
+            setHeight("300px");
+            setWidth("300px");
+            setPositionY(50);
+            setPositionX(50);
+
+
+            Button updateButton = new Button("Update", new Button.ClickListener() {
+                private static final long serialVersionUID = 2181474159749122119L;
+
+                @Override
+                public void buttonClick(Button.ClickEvent event) {
+
+                    close();
+
+
+                }
+            });
+            updateButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+            VerticalLayout itemplacement = new VerticalLayout();
+            itemplacement.setSizeFull();
+            itemplacement.addComponent(updateButton);
+            main.addComponent(itemplacement);
+
+        }
 
     }
 
@@ -131,65 +243,63 @@ public class ProjectManagementPage extends HorizontalLayout implements View{
         container.setExpandRatio(panel, 1f);
     }
 
-    public class ProjectItem extends HorizontalLayout{
+    public class ProjectItem extends HorizontalLayout {
         public String manager;
         public Button sprintButton;
 
-        public ProjectItem(String name) {
-            manager =  (String) VaadinSession.getCurrent().getAttribute("user");
+        public ProjectItem(String name, String manager) {
+
             setSizeFull();
             HorizontalLayout container = new HorizontalLayout();
             final VerticalLayout nextcontainer = new VerticalLayout();
-            nextcontainer.addStyleName("projectPanel");
+            nextcontainer.addStyleName("projectContainer");
             //nextcontainer.setHeight("200px");
             //nextcontainer.setSpacing(true);
             Panel ProjectPanel = new Panel(name);
+            ProjectPanel.addStyleName("projectPanel");
             ProjectPanel.setWidth("100%");
             ProjectPanel.setContent(nextcontainer);
             container.setWidth("100%");
             addComponent(container);
-            sprintButton = new Button("Sprint", new Button.ClickListener() {
+            Button addSprintButton = new Button("Add sprint..", new Button.ClickListener() {
                 private static final long serialVersionUID = 2181474159879122119L;
-                @Override
-                public void buttonClick(Button.ClickEvent event) {
-                    SprintViewObject sprint = new SprintViewObject();
-                    nextcontainer.addComponent(sprint.ProjectButton);
-                    sprint.ProjectButton.addStyleName("ProjectButton");
-                    sprint.ProjectButton.setWidth("100%");
-                }
-            });
-            container.addComponent(ProjectPanel);
-            container.addComponent(sprintButton);
-        }
 
-        public ProjectItem(String name, String manager) {
-            setSizeFull();
-            HorizontalLayout container = new HorizontalLayout();
-            final VerticalLayout nextcontainer = new VerticalLayout();
-            nextcontainer.setHeight("250px");
-            nextcontainer.setSpacing(true);
-            Panel ProjectPanel = new Panel(name);
-            ProjectPanel.setWidth("100%");
-            ProjectPanel.setContent(nextcontainer);
-            container.setWidth("100%");
-            addComponent(container);
-            sprintButton = new Button("Sprint", new Button.ClickListener() {
-                private static final long serialVersionUID = 2181474159879122119L;
                 @Override
                 public void buttonClick(Button.ClickEvent event) {
-                    SprintViewObject sprint = new SprintViewObject();
-                    nextcontainer.addComponent(sprint.ProjectButton);
-                    sprint.ProjectButton.addStyleName("ProjectButton");
-                    sprint.ProjectButton.setWidth("100%");
+
+                    createNewSprint = new addSprintWindow(nextcontainer);
+                    UI.getCurrent().addWindow(createNewSprint);
+
+                }
+
+
+            });
+
+            Button manageProjectButton = new Button("Manage project", new Button.ClickListener() {
+                @Override
+                public void buttonClick(Button.ClickEvent clickEvent) {
+                    //Method to display project creation popup called here
+                    manageProject = new manageProjectWindow();
+                    UI.getCurrent().addWindow(manageProject);
                 }
             });
             container.addComponent(ProjectPanel);
-            container.addComponent(sprintButton);
+
+            final HorizontalLayout buttonsContainer = new HorizontalLayout();
+            buttonsContainer.addComponent(manageProjectButton);
+            buttonsContainer.addComponent(addSprintButton);
+
+
+            nextcontainer.addComponent(buttonsContainer);
+
         }
     }
 
+
+
+
     public class SprintViewObject {
-        public final Button ProjectButton = new Button("Sprint", new Button.ClickListener() {
+        public final Button SprintButton = new Button("Sprint", new Button.ClickListener() {
             private static final long serialVersionUID = 2181474159879122119L;
             @Override
             public void buttonClick(Button.ClickEvent event) {
